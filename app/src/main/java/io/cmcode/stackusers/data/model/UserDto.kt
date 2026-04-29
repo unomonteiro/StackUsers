1b7e54d709a0c8373af96cdcc21dda00ea73bf44
Carlos Monteiro <unomonteiro@gmail.com>
2026-04-29 15:51:00 +0100

    fix wrong json field name for display_name

--- app/src/main/java/io/cmcode/stackusers/data/model/UserDto.kt
+++ app/src/main/java/io/cmcode/stackusers/data/model/UserDto.kt
@@ -5,7 +5,7 @@ import io.cmcode.stackusers.domain.model.User
 
 data class UserDto(
     @SerializedName("user_id") val userId: Int,
-    @SerializedName("display_name") val displeyName: String,
+    @SerializedName("display_name") val displayName: String,
     @SerializedName("reputation") val reputation: Int,
     @SerializedName("profile_image") val profileImage: String?,
     @SerializedName("location") val location: String?,
@@ -14,7 +14,7 @@ data class UserDto(
 ) {
     fun toDomainModel(): User = User(
         userId = userId.toString(),
-        displayName = displeyName,
+        displayName = displayName,
         reputation = reputation,
         profileImage = profileImage.orEmpty(),
         location = location,
